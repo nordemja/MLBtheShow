d9c6e3edb0c5a36d31916e79beab5215d3bbed05
Justin Nordeman <nordemja@mail.uc.edu>
2025-02-17 22:35:46 -0500

    fixes

--- getRankedSeasonsDataHITTER.py
+++ getRankedSeasonsDataHITTER.py
@@ -7,32 +7,62 @@ import pandas as pd
 
 
 def getHeaders():
-    filename = 'headers.json'
+    filename = "headers.json"
     with open(filename) as f:
         headers = json.load(f)
     return headers
 
+
 headers = getHeaders()
 
 
-getProfile = requests.get('https://mlb21.theshow.com/universal_profiles/angry_platypus62/game_history?mode=ARENA', headers=headers)
-soup = BeautifulSoup(getProfile.text, 'html.parser')
+getProfile = requests.get(
+    "https://mlb21.theshow.com/universal_profiles/angry_platypus62/game_history?mode=ARENA",
+    headers=headers,
+)
+soup = BeautifulSoup(getProfile.text, "html.parser")
 
-totalPages = soup.find('div', {'class': 'pagination'})
-totalPages = totalPages.find_all('a')
-totalPages =  int(totalPages[3].text)
+totalPages = soup.find("div", {"class": "pagination"})
+totalPages = totalPages.find_all("a")
+totalPages = int(totalPages[3].text)
 print(totalPages)
 
-with open('C:\\Users\\justi\\OneDrive\\Documents\\Python Programming\\MLBtheShow\\rankedSeasonsStatsHITTER.csv', 'w', newline='', encoding='utf-8') as csvFile:
-    statsFile = csv.DictWriter(csvFile, fieldnames=['Date','Stadium','Lineup', 'Position', 'Player', 'AB', 'R', 'H', 'RBI', 'BB', 'SO', 'AVG'])
+with open(
+    "C:\\Users\\justi\\OneDrive\\Documents\\Python Programming\\MLBtheShow\\rankedSeasonsStatsHITTER.csv",
+    "w",
+    newline="",
+    encoding="utf-8",
+) as csvFile:
+    statsFile = csv.DictWriter(
+        csvFile,
+        fieldnames=[
+            "Date",
+            "Stadium",
+            "Lineup",
+            "Position",
+            "Player",
+            "AB",
+            "R",
+            "H",
+            "RBI",
+            "BB",
+            "SO",
+            "AVG",
+        ],
+    )
     statsFile.writeheader()
-    for x in range(1, totalPages+1):
+    for x in range(1, totalPages + 1):
         while True:
             try:
-                print("page: " +str(x))
-                getProfile = requests.get('https://mlb21.theshow.com/universal_profiles/angry_platypus62/game_history?page='+str(x)+'&mode=ARENA', headers=headers)
-                soup = BeautifulSoup(getProfile.text, 'html.parser')
-                getGames = soup.findAll('div', {'class': 'mlb21-games-box'})
+                print("page: " + str(x))
+                getProfile = requests.get(
+                    "https://mlb21.theshow.com/universal_profiles/angry_platypus62/game_history?page="
+                    + str(x)
+                    + "&mode=ARENA",
+                    headers=headers,
+                )
+                soup = BeautifulSoup(getProfile.text, "html.parser")
+                getGames = soup.findAll("div", {"class": "mlb21-games-box"})
             except:
                 input("press enter: ")
                 continue
@@ -41,14 +71,14 @@ with open('C:\\Users\\justi\\OneDrive\\Documents\\Python Programming\\MLBtheShow
         for each in getGames:
             while True:
                 try:
-                    url = each.findAll('a')
+                    url = each.findAll("a")
                     url = url[-1]
-                    url =  'https://mlb21.theshow.com' + url['href']
+                    url = "https://mlb21.theshow.com" + url["href"]
                     onlineMatch = requests.get(url, headers=headers)
-                    soup = BeautifulSoup(onlineMatch.text, 'html.parser')
-                    onlineMatch = soup.find('div', {'class': 'section-block'})
-                    onlineMatch = onlineMatch.find('table')
-                    innings = onlineMatch.findAll('th')
+                    soup = BeautifulSoup(onlineMatch.text, "html.parser")
+                    onlineMatch = soup.find("div", {"class": "section-block"})
+                    onlineMatch = onlineMatch.find("table")
+                    innings = onlineMatch.findAll("th")
                 except:
                     input("press enter")
                     continue
@@ -56,54 +86,56 @@ with open('C:\\Users\\justi\\OneDrive\\Documents\\Python Programming\\MLBtheShow
 
             i = -1
             for each in innings:
-                while innings[i].text == '':
+                while innings[i].text == "":
                     i -= 1
             innings = int(innings[i].text)
 
             while True:
                 try:
-                    onlineMatch = onlineMatch.findAll('tr')
-                    awayTeam = onlineMatch[1].findAll('td')[2].text.strip()
-                    homeTeam = onlineMatch[2].findAll('td')[2].text.strip()
+                    onlineMatch = onlineMatch.findAll("tr")
+                    awayTeam = onlineMatch[1].findAll("td")[2].text.strip()
+                    homeTeam = onlineMatch[2].findAll("td")[2].text.strip()
                 except:
                     input("press enter: ")
                     continue
                 break
 
-            if (awayTeam != 'CPU' or homeTeam != 'CPU') and innings >= 7:
+            if (awayTeam != "CPU" or homeTeam != "CPU") and innings >= 7:
 
                 while True:
                     try:
 
-                        summaryList = soup.find('div', {'class': 'well'}).text.strip().split()
+                        summaryList = (
+                            soup.find("div", {"class": "well"}).text.strip().split()
+                        )
                         summaryList = summaryList[1:3]
 
-                        stadium = soup.findAll('div', {'class': 'section-block'})
+                        stadium = soup.findAll("div", {"class": "section-block"})
                         stadium = stadium[3]
                     except:
                         input("Press enter: ")
                         continue
                     break
                 g = []
-                for br in stadium.findAll('br'):
+                for br in stadium.findAll("br"):
                     next_s = br.nextSibling
-                    if not (next_s and isinstance(next_s,NavigableString)):
+                    if not (next_s and isinstance(next_s, NavigableString)):
                         continue
                     next2_s = next_s.nextSibling
-                    if next2_s and isinstance(next2_s,Tag) and next2_s.name == 'br':
+                    if next2_s and isinstance(next2_s, Tag) and next2_s.name == "br":
                         text = str(next_s).strip()
                         g.append(text)
 
-                stadium = g[-9].split('(')[0]
+                stadium = g[-9].split("(")[0]
 
-                boxScore = soup.findAll('div', {'class': 'mlb20-boxscore-box'})
-                if awayTeam == 'CPU':
-                    myBox = boxScore[0].find('table')
+                boxScore = soup.findAll("div", {"class": "mlb20-boxscore-box"})
+                if awayTeam == "CPU":
+                    myBox = boxScore[0].find("table")
                 else:
-                    myBox = boxScore[1].find('table')
-                
-                playerInfo = myBox.find('tbody')
-                playerInfo = playerInfo.findAll('tr')
+                    myBox = boxScore[1].find("table")
+
+                playerInfo = myBox.find("tbody")
+                playerInfo = playerInfo.findAll("tr")
 
                 pitcherAB = 0
                 pitcherR = 0
@@ -113,16 +145,16 @@ with open('C:\\Users\\justi\\OneDrive\\Documents\\Python Programming\\MLBtheShow
                 pitcherSO = 0
                 lineupPos = 1
                 for each in playerInfo:
-                    playerStats = each.findAll('td')
+                    playerStats = each.findAll("td")
                     player = playerStats[0].text
-                    position = player.split(',')[-1].strip()
+                    position = player.split(",")[-1].strip()
 
                     lineupPosTemp = lineupPos
                     if player[0].isupper() == False:
                         player = player[2:]
-                        lineupPos= 50
+                        lineupPos = 50
                         lineupPosTemp -= 1
-                    if player.split(',')[-1] == ' P':
+                    if player.split(",")[-1] == " P":
                         pitcherAB += int(playerStats[1].text)
                         pitcherR += int(playerStats[2].text)
                         pitcherH += int(playerStats[3].text)
@@ -131,7 +163,7 @@ with open('C:\\Users\\justi\\OneDrive\\Documents\\Python Programming\\MLBtheShow
                         pitcherSO += int(playerStats[6].text)
 
                     else:
-                        player = player.split(',')[0]
+                        player = player.split(",")[0]
                         AB = playerStats[1].text
                         R = playerStats[2].text
                         H = playerStats[3].text
@@ -139,11 +171,41 @@ with open('C:\\Users\\justi\\OneDrive\\Documents\\Python Programming\\MLBtheShow
                         BB = playerStats[5].text
                         SO = playerStats[6].text
 
-                        if player != 'Batting Totals':
+                        if player != "Batting Totals":
                             AVG = playerStats[7].text
-                            statsFile.writerow({'Date': summaryList[0], 'Stadium': stadium, 'Lineup': lineupPos, 'Position': position, 'Player': player, 'AB': AB, 'R': R, 'H': H, 'RBI': RBI, 'BB': BB, 'SO': SO, 'AVG': AVG})
+                            statsFile.writerow(
+                                {
+                                    "Date": summaryList[0],
+                                    "Stadium": stadium,
+                                    "Lineup": lineupPos,
+                                    "Position": position,
+                                    "Player": player,
+                                    "AB": AB,
+                                    "R": R,
+                                    "H": H,
+                                    "RBI": RBI,
+                                    "BB": BB,
+                                    "SO": SO,
+                                    "AVG": AVG,
+                                }
+                            )
                         else:
-                            AVG = float(H)/float(AB)
-                            statsFile.writerow({'Date': summaryList[0], 'Stadium': stadium, 'Lineup': 100, 'Position': 'TOTALS', 'Player': player, 'AB': int(AB)-int(pitcherAB), 'R': int(R)-int(pitcherR), 'H': int(H)-int(pitcherH), 'RBI': int(RBI)-int(pitcherRBI), 'BB': int(BB)-int(pitcherBB), 'SO': int(SO)-int(pitcherSO), 'AVG': AVG})
+                            AVG = float(H) / float(AB)
+                            statsFile.writerow(
+                                {
+                                    "Date": summaryList[0],
+                                    "Stadium": stadium,
+                                    "Lineup": 100,
+                                    "Position": "TOTALS",
+                                    "Player": player,
+                                    "AB": int(AB) - int(pitcherAB),
+                                    "R": int(R) - int(pitcherR),
+                                    "H": int(H) - int(pitcherH),
+                                    "RBI": int(RBI) - int(pitcherRBI),
+                                    "BB": int(BB) - int(pitcherBB),
+                                    "SO": int(SO) - int(pitcherSO),
+                                    "AVG": AVG,
+                                }
+                            )
                         lineupPos = lineupPosTemp
                         lineupPos += 1
