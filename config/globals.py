c8602a6f93fc97e5c85d71a30dd7a2f997d092fc
Justin Nordeman <nordemja@mail.uc.edu>
2025-04-23 00:15:21 -0400

    fixing error prohibitting captcha class from intalizing correctly

--- config/globals.py
+++ config/globals.py
@@ -10,5 +10,3 @@ COMMUNITY_MARKET_PATH: Final[str] = "https://mlb23.theshow.com/community_market"
 COMPLETED_ORDERS_PATH: Final[str] = "https://mlb23.theshow.com/orders/completed_orders"  # noqa: E501
 OPEN_BUY_ORDERS_PATH: Final[str] = "https://mlb23.theshow.com/orders/buy_orders"
 OPEN_SELL_ORDERS_PATH: Final[str] = "https://mlb23.theshow.com/orders/sell_orders"
-CAPTCHA_SOLVER_SEND_LINK: Final[str] = f"https://2captcha.com/in.php?key={api_key}&method=userrecaptcha&googlekey={data_sitekey}&pageurl={player_url}&json=1&invisible=1"   # noqa: E501
-CAPTCHA_SOLVER_GET_TOKEN_LINK: Final[str] = f"https://2captcha.com/res.php?key={api_key}&action=get&id={request_id}&json=1"   # noqa: E501
